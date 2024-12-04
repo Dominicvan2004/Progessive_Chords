e3d316b45a12366297ca9470c8b2c2ed5a796cea
Jork-MT361 <150489590+Jork-MT361@users.noreply.github.com>
2024-12-04 15:04:27 -0800

    Update countdown.js

--- website/javascript/countdown.js
+++ website/javascript/countdown.js
@@ -14,22 +14,22 @@ function updateCountdown() {
         
         if (timeRemaining <= 0) {
             countdownElement.textContent = "Event is over";
-            return; // Skip the rest of the code for this event
+            return; 
         }
 
        
         const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
         const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+        
 
-        // Update the countdown text
-        countdownElement.textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        
+        countdownElement.textContent = `${days}d ${hours}h ${minutes}m `;
     });
 }
 
 
-setInterval(updateCountdown, 1000);
+setInterval(updateCountdown, 100);
 
 
 updateCountdown();
